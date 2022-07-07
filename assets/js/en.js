28f9fdaf8cf5bb7dcad58be1e9efaceffe65834a
Mutlaq Aldhbuiub <matulkn@gmail.com>
2022-07-07 11:25:06 +0300

    Adding english lang.

---
+++ assets/js/en.js
@@ -0,0 +1,56 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+var imageWidth = 1080;
+var imageHeight = 1920;
+
+var imageObj = new Image(imageWidth,imageHeight);
+
+imageObj.onload = function()
+{
+    context.drawImage(imageObj, 0, 0);
+}
+
+function DownloadCanvasAsImage(){
+    let imageName = "moa3aydah.png";
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', imageName);
+    let canvas = document.getElementById('myCanvas');
+    canvas.toBlob(function(blob) {
+    let url = URL.createObjectURL(blob);
+    downloadLink.setAttribute('href', url);
+    downloadLink.click();
+    });
+}
+
+imageObj.src = "assets/images/en.jpg"; 
+
+var downloadCardButton = document.getElementById('downloadCard');
+downloadCardButton.addEventListener('click', function(e){
+    // clear canvas from text and draw image Regards,
+    context.clearRect(0, 0, imageWidth, imageHeight);
+    context.drawImage(imageObj, 0, 0);
+
+    var text = document.getElementById('name').value;
+
+    // custom font
+    context.textAlign = 'center';
+    context.font = "40pt Bahij";
+
+    // text color
+    context.fillStyle = '#535BAE';
+
+    // center and make text
+    textWidth = (imageWidth - imageWidth / 1.95)
+    textHeight = (imageHeight - 640)
+    
+    context.fillText(text, textWidth, textHeight);
+
+    e.preventDefault();
+    document.getElementById('name').value = "";
+
+    // download the image
+    DownloadCanvasAsImage();
+});
+
+
