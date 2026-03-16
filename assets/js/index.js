d6dd62a5c3e94cdcf1ec69401d030e398d072a86
Atheer Alotaibi <atheerf.otaibi@gmail.com>
2026-03-16 11:33:23 +0300

    -

--- assets/js/index.js
+++ assets/js/index.js
@@ -173,17 +173,12 @@ function drawCardWithText(messageText, nameText) {
   context.fillStyle = "#006E57";
   var centerX = canvasWidth / 2;
   var nameY = canvasHeight - 280;
-  var messageMaxWidth = 620;
+  var messageMaxWidth = 820;
   var messageLineHeight = 44;
 
   if (messageText) {
     context.font = "300 28pt Satorp";
     var messageLines = wrapCanvasText(context, messageText, messageMaxWidth);
-    if (messageLines.length > 2) {
-      context.font = "300 22pt Satorp";
-      messageLines = wrapCanvasText(context, messageText, messageMaxWidth);
-      messageLineHeight = 38;
-    }
     var maxMessageLines = 6;
     if (messageLines.length > maxMessageLines) messageLines = messageLines.slice(0, maxMessageLines);
     var messageY = nameY - 120 - (messageLines.length - 1) * messageLineHeight;
